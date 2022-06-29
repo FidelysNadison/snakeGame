4c3599357e9ac75e73887ab4ce0dc44cfd5eee07
Nadison Fidely <fidelysnad@exemple.com>
2022-06-29 12:07:53 +0200

    quelque changement au niveau js

---
+++ Assets/Script/classes/Block.js
@@ -0,0 +1,11 @@
+class Block {
+    constructor(x, y, size){
+        this.x = x;
+        this.y = y;
+        this.oldX = x;
+        this.oldY = y;
+        this.size = size;
+    }
+
+    teleportIfOutOfMap ( {})
+}
